498caf97511befbdc3c222175065514d89336e57
tokikuch <k.toshihito@yahoo.de>
2015-12-08 00:12:57 -0800

    simple spring-mass model

--- ems.py
+++ ems.py
@@ -1,62 +1,62 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.spatial import distance
 
 class Node:
-    def __init__(self, initpos):
-        self.friction = 0
+    def __init__(self, initpos, friction):
         self.m = 1
+        self.friction = friction
+        self.acc = np.array([0, 0], dtype='d')
         self.vel = np.array([0, 0], dtype='d')
         self.pos = np.array(initpos, dtype='d')
-        self.force = lambda : np.array([0, 0])
+        self.forceGenerators = []
+        self.history = np.empty((7, 0), dtype='d')
 
-    def dump2D(self):
-        print "[%+.3f %+.3f] v[%+.3f %+.3f] a[%+.3f %+.3f]" % \
-            (self.pos[0], self.pos[1],
-             self.vel[0], self.vel[1],
-             self.acc[0], self.acc[1])
-
-    def setFriction(self, u):
-        self.friction = u
-        
-    def getForceFixed(self, f):
+    def getFixedForceGenerator(node, f):
         return lambda : np.array([0, f])
-
-    def getForceSpring(self, k):
-        return lambda : np.array([0, -k * self.pos[1]])
-        
-    def setForce(self, force):
-        self.force = force
     
+    def getSpringForceGenerator(nodeFrom, nodeTo, k, l):
+        return lambda : k * (1 - l / distance.euclidean(nodeFrom.pos, nodeTo.pos)) * \
+            (nodeTo.pos - nodeFrom.pos)
+
+    def addForceGenerator(self, forceGenerator):
+        self.forceGenerators.append(forceGenerator)
+
+    def addSpring(n1, n2, k, l):
+        n1.addForceGenerator(Node.getSpringForceGenerator(n1, n2, k, l))
+        n2.addForceGenerator(Node.getSpringForceGenerator(n2, n1, k, l))
+
     def energy(self):
         return .5 * self.m * np.sum(self.vel * self.vel)
 
     def move(self, dt):
-        self.acc = self.force()
+        force = np.array([0, 0], dtype='d')
+        for forceGenerator in self.forceGenerators:
+            force += forceGenerator()
+        self.acc = force / self.m # for history logging
         self.pos += dt * self.vel
-        self.vel += dt * (self.force() - self.friction * self.vel) / self.m
+        self.vel += dt * (force - self.friction * self.vel) / self.m
 
-    def statvector(self):
-        return np.array([self.pos[1], self.vel[1], self.acc[1]])
+    def log(self, t):
+        self.history = np.c_[self.history, \
+                             np.r_[t, self.pos, self.vel, self.acc]]
 
 class Plotting:
     def __init__(self):
-        # data = [t, x, v, a]
-        self.data = np.empty((4, 0), dtype='d')
         self.styles = ['k', '#ff00ff', 'b']
 
-    def add(self, newvector):
-        self.data = np.c_[self.data, newvector]
+    def addplot(self, node):
+        # plt.plot(node.history[0], node.history[1], 'k', linewidth=2) # x
+        plt.plot(node.history[0], node.history[2], 'k', linewidth=2) # y
+        # plt.plot(node.history[0], node.history[3], 'k', linewidth=2) # vx
+        # plt.plot(node.history[0], node.history[4], 'k', linewidth=2) # vy
+        # plt.plot(node.history[0], node.history[5], 'k', linewidth=2) # ax
+        # plt.plot(node.history[0], node.history[6], 'k', linewidth=2) # ay
 
-    def show(self):
-        for row, style in zip(self.data[1:,:], self.styles):
-            plt.plot(self.data[0,:], row, style, linewidth=2)
-        plt.show()
-        
 class Field:
     def __init__(self):
         self.nodes = []
-        self.plot = Plotting()
-        
+
     def add(self, node):
         self.nodes.append(node)
         
@@ -66,24 +66,26 @@ class Field:
         for i in xrange(0, 1000):
             e = .0
             for node in self.nodes:
-                # node.dump2D()
-                node.move(dt)
+                node.log(t)
                 e += node.energy()
-                t += dt
-                self.plot.add(np.r_[t, node.statvector()])
-        self.plot.show()
+                node.move(dt)
+            print "t = %3.3f e = %.6f" % (t,  e)
+            t += dt
+
+        plot = Plotting()
+        for node in self.nodes:
+            plot.addplot(node)
+
+        plt.show()
 
 def Main():
-    n = Node([0, 1])
-
-    n.setFriction(.1)
-    n.setForce(n.getForceFixed(2))
-
-    #n.setFriction(1)
-    #n.setForce(n.getForceSpring(2))
+    n1 = Node([0, 0], 1)
+    n2 = Node([0, 2], 1)
+    Node.addSpring(n1, n2, k=2, l=1)
     
     field = Field()
-    field.add(n)
+    field.add(n1)
+    field.add(n2)
     field.run()
 
 if __name__ == '__main__':
