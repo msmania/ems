868b683b1fdbcba3b65aa5bc3c7a96a15d46e2ab
tokikuch <k.toshihito@yahoo.de>
2015-12-11 10:20:43 -0800

    implemented scatter animation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EMS - Euler Method Simulation
 ### Prereq
 1. sudo apt-get install python-numpy python-scipy python-matplotlib
-
+2. sudo apt-get install ffmpeg
--- ems.py
+++ ems.py
@@ -1,92 +1,87 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import animation as animation
 from scipy.spatial import distance
 
-class Node:
-    def __init__(self, initpos, friction):
-        self.m = 1
-        self.friction = friction
-        self.acc = np.array([0, 0], dtype='d')
-        self.vel = np.array([0, 0], dtype='d')
-        self.pos = np.array(initpos, dtype='d')
-        self.forceGenerators = []
-        self.history = np.empty((7, 0), dtype='d')
+class Field:
+    def __init__(self, dim=2):
+        self.dim = dim
+        self.m = []
+        self.friction = []
+        self.forces = []
+        self.acc = np.empty((0, dim), dtype='d')
+        self.vel = np.empty((0, dim), dtype='d')
+        self.pos = np.empty((0, dim), dtype='d')
+        self.fig, self.ax = plt.subplots()
 
-    def getFixedForceGenerator(node, f):
-        return lambda : np.array([0, f])
-    
-    def getSpringForceGenerator(nodeFrom, nodeTo, k, l):
-        return lambda : k * (1 - l / distance.euclidean(nodeFrom.pos, nodeTo.pos)) * \
-            (nodeTo.pos - nodeFrom.pos)
+    def addSpring(self, n1, n2, k, l):
+        self.forces[n1].append(self.getSpringForceGenerator(n1, n2, k, l))
+        self.forces[n2].append(self.getSpringForceGenerator(n2, n1, k, l))
 
-    def addForceGenerator(self, forceGenerator):
-        self.forceGenerators.append(forceGenerator)
-
-    def addSpring(n1, n2, k, l):
-        n1.addForceGenerator(Node.getSpringForceGenerator(n1, n2, k, l))
-        n2.addForceGenerator(Node.getSpringForceGenerator(n2, n1, k, l))
-
-    def energy(self):
-        return .5 * self.m * np.sum(self.vel * self.vel)
+    def getSpringForceGenerator(self, n1, n2, k, l):
+        return lambda : k * (1 - l / distance.euclidean(self.pos[n1,:],
+                                                        self.pos[n2,:])) * \
+                            np.array(self.pos[n2,:] - self.pos[n1,:])
 
     def move(self, dt):
-        force = np.array([0, 0], dtype='d')
-        for forceGenerator in self.forceGenerators:
-            force += forceGenerator()
-        self.acc = force / self.m # for history logging
-        self.pos += dt * self.vel
-        self.vel += dt * (force - self.friction * self.vel) / self.m
+        self.acc = np.array([sum([f() for f in forces])
+                             for forces in self.forces]) / self.m
+        self.vel += dt * np.array(self.acc) - (self.friction * self.vel) / self.m
+        self.pos += dt * np.array(self.vel)
 
-    def log(self, t):
-        self.history = np.c_[self.history, \
-                             np.r_[t, self.pos, self.vel, self.acc]]
-
-class Plotting:
-    def __init__(self):
-        self.styles = ['k', '#ff00ff', 'b']
-
-    def addplot(self, node):
-        # plt.plot(node.history[0], node.history[1], 'k', linewidth=2) # x
-        plt.plot(node.history[0], node.history[2], 'k', linewidth=2) # y
-        # plt.plot(node.history[0], node.history[3], 'k', linewidth=2) # vx
-        # plt.plot(node.history[0], node.history[4], 'k', linewidth=2) # vy
-        # plt.plot(node.history[0], node.history[5], 'k', linewidth=2) # ax
-        # plt.plot(node.history[0], node.history[6], 'k', linewidth=2) # ay
-
-class Field:
-    def __init__(self):
-        self.nodes = []
-
-    def add(self, node):
-        self.nodes.append(node)
-        
-    def run(self):
-        t = .0
-        dt = .01
+    def frameGen(self):
+        dt = .1
         for i in xrange(0, 1000):
-            e = .0
-            for node in self.nodes:
-                node.log(t)
-                e += node.energy()
-                node.move(dt)
-            print "t = %3.3f e = %.6f" % (t,  e)
-            t += dt
+            self.move(dt)
+            yield self
 
-        plot = Plotting()
-        for node in self.nodes:
-            plot.addplot(node)
+    def initFrame(self):
+        data = next(self.frameGen())
+        x = data.pos[:,0]
+        y = data.pos[:,1]
+        self.scat = self.ax.scatter(x, y)
+        l = 5
+        self.ax.axis([-l, l, -l, l])
+        return self.scat,
 
-        plt.show()
+    def updateFrame(self, i):
+        data = next(self.frameGen())
+        self.scat.set_offsets(data.pos)
+        return self.scat,
+
+    def dump(self, i):
+        print '%4d (%+.3f, %+.3f) v(%+.3f, %+.3f) a(%.3f, %.3f)' % \
+            (i,
+             self.pos[i,0], self.pos[i,1],
+             self.vel[i,0], self.vel[i,1],
+             self.acc[i,0], self.acc[i,1])
+
+    def add(self, pos, friction=1, m=1):
+        self.m.append(m)
+        self.friction.append(friction)
+        self.forces.append([])
+        self.acc = np.vstack([self.acc, np.zeros(self.dim)])
+        self.vel = np.vstack([self.vel, np.zeros(self.dim)])
+        self.pos = np.vstack([self.pos, pos])
+        
+    def start(self):
+        self.anime = animation.FuncAnimation(self.fig,
+                                             self.updateFrame,
+                                             init_func=self.initFrame,
+                                             interval=50)
+
+    def save(self, filename):
+        self.anime.save(filename)
 
 def Main():
-    n1 = Node([0, 0], 1)
-    n2 = Node([0, 2], 1)
-    Node.addSpring(n1, n2, k=2, l=1)
-    
-    field = Field()
-    field.add(n1)
-    field.add(n2)
-    field.run()
+    field = Field(dim=2)
+    field.add([0, 4], .04)
+    field.add([0, -4], .04)
+    field.addSpring(0, 1, k=2, l=5)
+    field.start()
+    #field.save('ems.mp4')
+    print 'Done!'
+    plt.show()
 
 if __name__ == '__main__':
     Main()
